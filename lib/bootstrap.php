a5c57832447910656b5b5a7611b87783c5f57370
Anthony Ferrara <ircmaxell@gmail.com>
2015-03-30 10:05:05 -0400

    Implement generic composer scanning which will recursively look at parent directories for a composer autoloader

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,6 +1,15 @@
 <?php
 
-$autoloader = require __DIR__ . "/../vendor/autoload.php";
+$dir = dirname(__DIR__);
+while ($dir !== DIRECTORY_SEPARATOR && !file_exists($dir . "/vendor/autoload.php")) {
+	$dir = dirname($dir);
+}
+
+if ($dir === DIRECTORY_SEPARATOR) {
+	throw new \RuntimeException("Cannot find autoloader, did you install with composer?");
+}
+
+$autoloader = require $dir . "/vendor/autoload.php";
 $new_autoloader = new PhpGenerics\Autoloader($autoloader);
 $autoloader->unregister();
 $new_autoloader->register(true);
